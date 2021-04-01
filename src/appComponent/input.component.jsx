3307f6e9ddf16e271a1fabce686cd2333c97ccb0
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2021-04-01 08:36:29 +0200

    Fix

--- src/appComponent/input.component.jsx
+++ src/appComponent/input.component.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import './input.style.css'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faSearch } from "@fortawesome/free-solid-svg-icons";
 const Form = properties => {
     return (
         <div className="container">
-            <div>{properties.error ? error(): null}</div>
-            <form onSubmit={properties.loadWeather}>
-                <div className="row">
-                    <div className="col-md-3 offset-md-3">
-                        <input type="text" className="from-control" name="city" autoComplete="off" placeholder="City" />
+            <div className="col-md-6 center">
+                <div>{properties.error ? error() : null}</div>
+                <form onSubmit={properties.loadWeather}>
+                    <div className="input-group rounded">
+                        <input type="search" type="text" className="form-control rounded" placeholder="Search" aria-label="Search"
+                            aria-describedby="search-addon" name="city" autoComplete="off" placeholder="City" />
+                        <button className="input-group-text border-0" id="search-addon">
+                            <i className="fas fa-search"></i>
+                            <FontAwesomeIcon icon={faSearch} />
+                        </button>
                     </div>
-                    <div className="col-md-3">
-                        <button className="btn btn-warning">Get</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    );
+    )
+    // return (
+    //     <div className="container">
+    //         <div>{properties.error ? error(): null}</div>
+    //         <form onSubmit={properties.loadWeather}>
+    //             <div className="row">
+    //                 <div className="col-md-3 offset-md-3">
+    //                     <input type="text" className="from-control" name="city" autoComplete="off" placeholder="City" />
+    //                 </div>
+    //                 <div className="col-md-3">
+    //                     <button className="btn btn-warning">Get</button>
+    //                 </div>
+    //             </div>
+    //         </form>
+    //     </div>
+    // );
 }
 
 
 function error() {
-    return(
+    return (
         <div className="alert alert-danger mx-5" role="alert">
-        Invalid Location.
+            Invalid Location.
         </div>
     );
 }
--- src/appComponent/weather.component.jsx
+++ src/appComponent/weather.component.jsx
@@ -14,7 +14,6 @@ const Weather = (properties) => {
             </div>
             <hr></hr>
         </div>
-        
     );
 }
 
